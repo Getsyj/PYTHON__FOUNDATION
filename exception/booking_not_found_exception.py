6dbf181199f0f19040a01dafb3220f502c0649ba
Getsyj <114157118+Getsyj@users.noreply.github.com>
2025-06-30 15:43:28 +0530

    Delete exception directory

--- exception/booking_not_found_exception.py
+++
@@ -1,4 +0,0 @@
-class BookingNotFoundException(Exception):
-    def __init__(self, message="Booking not found."):
-        self.message = message
-        super().__init__(self.message)
--- exception/passenger_not_found_exception.py
+++
@@ -1,3 +0,0 @@
-class PassengerNotFoundException(Exception):
-    def __init__(self, passenger_id: int):
-        super().__init__(f"Passenger with ID {passenger_id} not found.")
